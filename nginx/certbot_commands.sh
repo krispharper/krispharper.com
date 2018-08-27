d38e9bedae04872cb75bc428f802fe7db5c5085f
Kris Harper <kris.p.harper@gmail.com>
2018-08-27 19:26:14 +0000

    Added Jackett

---
+++ nginx/certbot_commands.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker exec nginx mkdir /etc/nginx/html
+docker exec -it nginx certbot certonly --webroot -w /etc/nginx/html -d krispharper.com -d www.krispharper.com -d plex.krispharper.com -d transmission.krispharper.com -d nas.krispharper.com -d paulaclareharper.krispharper.com -d paulaclareharper.com -d www.paulaclareharper.com -d phlex.krispharper.com -d sonarr.krispharper.com -d jackett.krispharper.com
