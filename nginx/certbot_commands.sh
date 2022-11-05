4c7aaad5546888012f19ecfdda9a8080707a371f
Kris Harper <kris.harper@stoneridgeam.com>
2022-11-05 21:14:14 +0000

    Updated certbot

--- nginx/certbot_commands.sh
+++ nginx/certbot_commands.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-docker exec nginx mkdir /etc/nginx/html
-docker exec -it nginx \
-    certbot-auto certonly \
-    --webroot \
+docker exec nginx mkdir -p /etc/nginx/html \
+&& docker exec -it nginx \
+    certbot certonly \
+    --nginx \
     -w /etc/nginx/html \
     -d krispharper.com \
     -d www.krispharper.com \
-    -d plex.krispharper.com \
-    -d transmission.krispharper.com \
     -d nas.krispharper.com \
-    -d paulaclareharper.krispharper.com \
-    -d paulaclareharper.com \
-    -d www.paulaclareharper.com \
-    -d moviematch.krispharper.com \
+    -d plex.krispharper.com \
     -d tautulli.krispharper.com \
+    -d moviematch.krispharper.com \
+    -d transmission.krispharper.com \
     -d sonarr.krispharper.com \
     -d jackett.krispharper.com \
     -d radarr.krispharper.com \
     -d crashplan.krispharper.com \
-    -d musicscholarshipatadistance.com \
+    -d pi-hole.krispharper.com \
+    -d paulaclareharper.krispharper.com \
+    -d paulaclareharper.com \
+    -d www.paulaclareharper.com \
+    -d musicscholarshipatadistance.krispharper.com \
     -d swiftconference2021.com \
-&& sudo systemctl nginx restart
+&& sudo systemctl restart nginx
